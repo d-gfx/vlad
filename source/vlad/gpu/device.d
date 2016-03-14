3d8c1e4b2138cf74e340a89a832fcd7d79ef8dc9
d_gfx <nomail>
2016-03-14 10:57:44 +0900

    adding queue family property

--- source/vlad/gpu/device.d
+++ source/vlad/gpu/device.d
@@ -11,6 +11,15 @@ version(Vulkan)
 	private import derelict.vulkan.vk;
 	private import derelict.vulkan.vulkan;
 	alias	Instance = VkInstance;
+
+	struct PhysicalDevice
+	{
+		VkPhysicalDevice					device;
+		VkPhysicalDeviceMemoryProperties	memory;
+		int									graphics_queue_index;
+		VkQueueFamilyProperties[]			queue_family_props;
+	}
+
 	bool setupApi()
 	{
 		DerelictVulkan.load();
@@ -62,6 +71,60 @@ version(Vulkan)
 		auto ret = vkCreateInstance(&instance_info, null, &inst);
 		return (ret == VkResult.VK_SUCCESS);
 	}
+	bool enumerateDevices(ref Instance inst, ref PhysicalDevice[] devices)
+	{
+		uint count = 0;
+		auto result = vkEnumeratePhysicalDevices(inst, &count, null);
+		if (result != VkResult.VK_SUCCESS || count < 1)
+		{
+			writeln("Error : vkEnumeratePhysicalDevices() Failed.");
+			return false;
+		}
+
+		devices.length = count;
+
+		VkPhysicalDevice[] tmp_devs;
+		tmp_devs.length = count;
+		scope (exit) tmp_devs.length = 0;
+
+		result = vkEnumeratePhysicalDevices(inst, &count, tmp_devs.ptr);
+		if (result != VkResult.VK_SUCCESS)
+		{
+			writeln("Error : vkEnumeratePhysicalDevices() Failed.");
+			return false;
+		}
+
+		foreach(i, dev; tmp_devs)
+		{
+			devices[i].device = dev;
+			vkGetPhysicalDeviceMemoryProperties(devices[i].device, &devices[i].memory);
+		}
+
+		// enumerate queue property
+		foreach (ref dev; devices)
+		{
+			// get queue family count
+			uint prop_count;
+			vkGetPhysicalDeviceQueueFamilyProperties(dev.device, &prop_count, null);
+
+			// allocate buffer
+			dev.queue_family_props.length = prop_count;
+
+			// store queue family property
+			vkGetPhysicalDeviceQueueFamilyProperties(dev.device, &prop_count, dev.queue_family_props.ptr);
+
+			// find graphics queue
+			foreach(i; 0..prop_count)
+			{
+				if (dev.queue_family_props[i].queueFlags & VkQueueFlagBits.VK_QUEUE_GRAPHICS_BIT)
+				{
+					dev.graphics_queue_index = i;
+				}
+			}
+		}
+
+		return true;
+	}
 }
 
 /**
@@ -80,6 +143,14 @@ public:
 		if (!mIsEnable)
 			return;
 
+		mIsEnable = enumerateDevices(mInstance, mDevices);
+		if (!mIsEnable)
+		{
+			finalize();
+			writeln("Error : GpuDevice::enumerateDevices failed.");
+			return;
+		}
+
 		writeln("GpuDevice::created.");
 	}
 
@@ -93,6 +164,7 @@ public:
 	}
 	bool isEnable() const { return mIsEnable; }
 private:
-	bool		mIsEnable;
-	Instance	mInstance;
+	bool				mIsEnable;
+	Instance			mInstance;
+	PhysicalDevice[]	mDevices;
 }
